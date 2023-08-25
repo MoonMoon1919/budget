ded318f5636a62f5a09045392726a9d77fb3a8a6
Maxwell Moon <moon.maxwell@gmail.com>
2023-08-25 06:56:14 -0700

    complete rough sketch requirements and associated tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,37 +4,42 @@
 
 use uuid::Uuid;
 
-use std::collections::HashMap;
+use std::{collections::HashMap, cell::RefCell};
 
 #[derive(Debug)]
 struct User {
-    budgets: HashMap<String, Budget>
+    budgets: HashMap<String, BudgetManager>
 }
 
 #[derive(Debug)]
-struct Budget {
+struct BudgetManager {
     user_id: String,
-    id: String,
     name: String,
-    transactions: Vec<Transaction>
+    budget: Budget,
+    transactions: RefCell<Vec<Transaction>>
 }
 
-impl Budget {
-    fn new(name: String, user_id: String) -> Self {
-        Budget {
-            user_id: user_id,
-            id: Uuid::new_v4().to_string(),
-            name: name,
-            transactions: vec![]
+impl BudgetManager {
+    fn new(name: String, user_id: String, budget: Budget) -> Self {
+        BudgetManager {
+            transactions: RefCell::new(vec![]),
+            user_id,
+            name,
+            budget
         }
     }
 
+    fn available_funds(&self) -> f64 {
+        self.budget.available_funds()
+    }
+
     fn add_tx(&mut self, tx: Transaction) {
-        self.transactions.push(tx);
+        self.budget.withdraw(&tx.value);
+        self.transactions.borrow_mut().push(tx);
     }
 
     fn find_tx_index(&self, id: &String) -> Result<usize, String> {
-        for (i, tx) in self.transactions.iter().enumerate() {
+        for (i, tx) in self.transactions.borrow().iter().enumerate() {
             if &tx.id == id {
                 return Ok(i)
             } else {
@@ -45,13 +50,59 @@ impl Budget {
         Err(String::from("Error, not found"))
     }
 
+    fn update_tx(&mut self, id: String, val: f64) {
+        let idx = match self.find_tx_index(&id) {
+            Ok(i) => i,
+            _ => panic!("Transaction not found")
+        };
+
+        let mut txs = self.transactions.borrow_mut();
+        let tx = txs.get_mut(idx).expect("Transaction not found");
+
+        self.budget.deposit(&tx.value);
+        tx.update_value(val);
+        self.budget.withdraw(&tx.value);
+    }
+
+
     fn remove_tx(&mut self, id: String) {
         let idx = match self.find_tx_index(&id) {
             Ok(i) => i,
-            _ => panic!("Index not found")
+            _ => panic!("Transaction not found")
         };
 
-        self.transactions.remove(idx);
+        let mut txs = self.transactions.borrow_mut();
+        let tx = txs.get(idx).expect("Transaction not found");
+
+        self.budget.deposit(&tx.value);
+        txs.remove(idx);
+    }
+}
+
+#[derive(Debug)]
+struct Budget {
+    id: String,
+    total: f64,
+}
+
+impl Budget {
+    fn new(total: f64) -> Self {
+        Budget {
+            id: Uuid::new_v4().to_string(),
+            total,
+        }
+    }
+
+    fn available_funds(&self) -> f64 {
+        self.total
+    }
+
+    fn withdraw(&mut self, val: &f64) {
+        self.total = self.total - val;
+    }
+
+    fn deposit(&mut self, val: &f64) {
+        self.total = self.total + val;
     }
 }
 
@@ -86,25 +137,64 @@ mod tests {
     use super::*;
 
     #[test]
-    fn budget_can_add_transaction() {
-        let mut budg = Budget::new(String::from("my-budget"), Uuid::new_v4().to_string());
+    fn budget_manager_can_add_transaction() {
+        let budg = Budget::new(200.00_f64);
+        let mut budgman = BudgetManager::new(String::from("my-budget"), Uuid::new_v4().to_string(), budg);
         let tx = Transaction::new(String::from("cheeseborger"), 3.99_f64);
 
-        budg.add_tx(tx);
+        budgman.add_tx(tx);
 
-        assert_eq!(budg.transactions.len(), 1)
+        assert_eq!(budgman.transactions.borrow().len(), 1);
+        assert_eq!(budgman.budget.available_funds(), 196.01_f64)
     }
 
     #[test]
-    fn budget_can_remove_transaction() {
-        let mut budg = Budget::new(String::from("my-budget"), Uuid::new_v4().to_string());
+    fn budget_manager_can_remove_transaction() {
+        let budg = Budget::new(200.00_f64);
+        let mut budgman = BudgetManager::new(String::from("my-budget"), Uuid::new_v4().to_string(), budg);
         let tx = Transaction::new(String::from("cheeseborger"), 3.99_f64);
         let tx_id = tx.id.clone();
 
-        budg.add_tx(tx);
+        budgman.add_tx(tx);
+        budgman.remove_tx(tx_id);
 
-        budg.remove_tx(tx_id);
+        assert_eq!(budgman.transactions.borrow().len(), 0);
+        assert_eq!(budgman.available_funds(), 200.00_f64);
+    }
 
-        assert_eq!(budg.transactions.len(), 0)
+    #[test]
+    fn budget_can_withdraw_money() {
+        let mut budget = Budget::new(200.00_f64);
+
+        budget.withdraw(&10.00_f64);
+
+        assert_eq!(budget.available_funds(), 190.00_f64)
+    }
+
+    #[test]
+    fn budget_can_deposit_money() {
+        let mut budget = Budget::new(200.00_f64);
+
+        budget.deposit(&10.00_f64);
+
+        assert_eq!(budget.available_funds(), 210.00_f64)
+    }
+
+    #[test]
+    fn tx_can_get_renamed() {
+        let mut tx = Transaction::new(String::from("cheeseborger"), 3.99_f64);
+
+        tx.rename(String::from("cheeseburger"));
+
+        assert_eq!(tx.name, String::from("cheeseburger"))
+    }
+
+    #[test]
+    fn tx_can_have_value_updated() {
+        let mut tx = Transaction::new(String::from("cheeseborger"), 3.99_f64);
+
+        tx.update_value(4.99_f64);
+
+        assert_eq!(tx.value, 4.99_f64)
     }
 }
